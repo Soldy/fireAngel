2e30f0379b958d8bdc77f1a072c98378e4452788
Soldy <4786022+Soldy@users.noreply.github.com>
2021-03-07 13:15:45 +0000

    iptables filter update
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ src/buildIptables.py
@@ -0,0 +1,32 @@
+priority = [
+    'raw',
+    'filter',
+    'mangle'
+]
+
+special = 
+
+def chain(name):
+    name.upper()
+    return ''.join([
+        ':', 
+        name,upper()
+        ' - [0:0]'
+    ])
+
+
+def chains(group, arr):
+    out = []
+    out.append(('*').join(group))
+    out.append(chain('INPUT ACCEPT'))
+    out.append(chain(' ACCEPT'))
+    FORWARD ACCEPT [0:0]
+    OUTPUT ACCEPT [249940:29998746]
+    for i in arr)
+        out.append(chain(i))
+    return out
+
+
+
+
+
